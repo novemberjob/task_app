327586c4a5980e131cc5fad9303d0a4f7fb7ca39
Brang Seng <novemberjob007@gmail.com>
2025-05-13 18:47:05 +0630

    Add delete task

--- app.py
+++ app.py
@@ -33,6 +33,19 @@ def index():
         tasks = MyTask.query.order_by(MyTask.created_at).all()   
         return render_template("index.html", tasks=tasks)
 
+#delete task  
+@app.route("/delete/<int:id>")
+def delete(id):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        db.session.rollback()
+        print(f"Error: {e}")
+        return f"Error: {e}"
+
 if __name__ in "__main__":
     with app.app_context():
         db.create_all()
